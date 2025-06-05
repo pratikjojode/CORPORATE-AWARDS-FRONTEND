586af286a095ffabaf0c675afbc199abf7319c3c
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-06-05 21:28:10 +0530

    okay

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -4,15 +4,17 @@ import "../Styles/HeroSection.css";
 import jobizaaLogo from "../assets/jobizza.jpg";
 import atbLogo from "../assets/anytime.jpg";
 
-import bg1 from "../assets/hero-bg2.jpg";
-import bg2 from "../assets/hero-bgg3.jpg";
-import bg3 from "../assets/IMG-20250412-WA0003.jpg";
-import bg4 from "../assets/hero-bg3.jpg";
+import bg1 from "../assets/job4.jpg";
+import bg2 from "../assets/job3.jpg";
+import bg3 from "../assets/job2.jpg";
+import bg4 from "../assets/job1.jpg";
+import bg5 from "../assets/job5.jpg";
+import bg6 from "../assets/job6.jpg";
 
 const HeroSection = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  const backgroundImages = [bg1, bg2, bg3, bg4];
+  const backgroundImages = [bg1, bg2, bg3, bg4, bg5, bg6];
 
   useEffect(() => {
     const buttons = document.querySelectorAll(".primary-btn, .secondary-btn");
